1a867da4af63de8abb695aa37c18dcf8e8b7b5e4
sugaranjan <sugaranjan06@gmail.com>
2023-05-04 14:13:44 +0000

    update main

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (FRont
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
