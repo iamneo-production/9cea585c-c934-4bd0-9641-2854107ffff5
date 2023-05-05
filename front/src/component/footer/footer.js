2f5bbc801191039a62c01d241fe91110542f1a7f
sugaranjan <sugaranjan06@gmail.com>
2023-05-05 07:03:56 +0000

    Footer Sample

---
+++ front/src/component/footer/footer.js
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <div className="container">
+          <span className="text-muted">Copyright © 2023</span>
+        </div>
+      </footer>
+    );
+  }
+export default Footer;
