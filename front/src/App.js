6b06c6cd57097ad04a89872e6e7066508bd0fae4
MuskanChhabraa <muskanchhabra2121@gmail.com>
2023-05-05 14:54:07 +0000

    commit

--- front/src/App.js
+++ front/src/App.js
@@ -1,44 +1,41 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Component/Home.js';
+import About from './Component/About';
+import Contact from './Component/Contact';
 import './App.css'
-  
-class App extends Component {
-  render() {
-    return (
-      <>
-      
-       <Router>
-           <div className="App">
 
-            <ul className="App-header" id='ul-nb'>
-              <li ><a>
-                <Link to="/">Home</Link>
-                </a>
-              </li>
-              <li><a>
-                <Link to="/about">About Us</Link>
-                </a>
-              </li>
-              <li><a>
-                <Link to="/contact">Contact Us</Link>
-                </a>
-              </li>
-              <button id='button'>Login/Register</button>
-            </ul>
+function Navbar() {
+  return (
+    <>
+      <Router>
+        <div className="App">
 
-           <Routes>
-                 <Route exact path='/' element={< Home />}></Route>
-                 <Route exact path='/about' element={< About />}></Route>
-                 <Route exact path='/contact' element={< Contact />}></Route>
+          <ul className="App-header" id='ul-nb'>
+            <li ><a>
+              <Link to="/">Home</Link>
+            </a>
+            </li>
+            <li><a>
+              <Link to="/about">About Us</Link>
+            </a>
+            </li>
+            <li><a>
+              <Link to="/contact">Contact Us</Link>
+            </a>
+            </li>
+            <button id='button'>Login/Register</button>
+          </ul>
+
+          <Routes>
+            <Route exact path='/' element={< Home />}></Route>
+            <Route exact path='/about' element={< About />}></Route>
+            <Route exact path='/contact' element={< Contact />}></Route>
           </Routes>
-          
-          </div>
-       </Router>
-       </>
-   );
-  }
+
+        </div>
+      </Router>
+    </>
+  );
 }
-export default App
+export default Navbar;
--- front/src/Component/Home.js
+++ front/src/Component/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Home (){
+function Home(){
     return (
     <h1>This is home</h1>
     )
