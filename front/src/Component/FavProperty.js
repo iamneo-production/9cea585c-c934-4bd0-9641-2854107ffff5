67c2bd224a0bd2812acee95bce22a380e7f42345
sugaranjan <sugaranjan06@gmail.com>
2023-05-22 02:47:20 +0000

    Commit

---
+++ front/src/Component/FavProperty.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const FavProperty= ()=>{
+    return(
+        
+            <h1>This is FavProperty</h1>
+        
+    )
+}
+
+export default FavProperty;
---
+++ front/src/Component/PropertyListing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const PropertyListing = ()=>{
+    return(
+        
+            <h1>This is PropertyListing</h1>
+        
+    )
+}
+
+export default PropertyListing;
