26eeffa41c7d4a074d8fee8d02553809cf976a2d
sugaranjan <sugaranjan06@gmail.com>
2023-06-27 08:47:56 +0000

    Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,32 +1,8 @@
-<<<<<<< HEAD
-=======
-import logo from './logo.svg';
->>>>>>> Project-Workspace-archanakarchana
 import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
   <h1>Team -32</h1>
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Hello World</h1>
-      </header>
-    </div>
->>>>>>> Project-Workspace-archanakarchana
   );
 }
 
