450d915d759f3c9efad648d41023e45225d954ac
MuskanChhabraa <muskanchhabra2121@gmail.com>
2023-07-16 16:12:34 +0000

    Property Controller

--- springapp/src/main/java/com/example/springapp/controller/PropertyController.java
+++ springapp/src/main/java/com/example/springapp/controller/PropertyController.java
@@ -1,10 +1,74 @@
 package com.example.springapp.controller;
 
-import org.springframework.web.bind.annotation.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.springapp.model.Agent;
+import com.example.springapp.model.Property;
+import com.example.springapp.repository.AgentRepository;
+import com.example.springapp.service.PropertyService;
 
 @RestController
 @RequestMapping("/properties")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*")
 public class PropertyController {
 
+    @Autowired
+    private PropertyService propertyService;
+
+    @Autowired
+    private AgentRepository agentRepository;
+
+    @Value("${property.media.path}")
+    private String mediaPath;
+
+    @PostMapping
+    public ResponseEntity<Property> registerProperty(
+            @ModelAttribute Property property,
+            @RequestParam("images") MultipartFile[] images,
+            @RequestParam("videos") MultipartFile[] videos,
+            @RequestParam("agentId") Long agentId) {
+
+        // Retrieve agent from the database
+        Agent agent = agentRepository.findById(agentId)
+                .orElseThrow(() -> new RuntimeException("Agent not found with id: " + agentId));
+
+        // Set the agent for the property
+        property.setAgent(agent);
+
+        // Set the image URLs
+        List<String> imageUrls = saveMediaFiles(images);
+        property.setImageUrls(imageUrls);
+
+        // Set the video URLs
+        List<String> videoUrls = saveMediaFiles(videos);
+        property.setVideoUrls(videoUrls);
+
+        Property createdProperty = propertyService.createProperty(property);
+
+        if (createdProperty != null) {
+            return new ResponseEntity<>(createdProperty, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
