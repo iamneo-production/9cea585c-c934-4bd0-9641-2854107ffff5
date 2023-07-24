af35327495d4d040c812a0020c1bf67bebdaec9e
sugaranjan <sugaranjan06@gmail.com>
2023-07-24 15:09:14 +0000

    Integration

--- springapp/src/main/java/com/example/springapp/config/JwtUtil.java
+++ springapp/src/main/java/com/example/springapp/config/JwtUtil.java
@@ -3,17 +3,26 @@ package com.example.springapp.config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private static String secretKeyString = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY4OTgzNTYxMiwiaWF0IjoxNjg5ODM1NjEyfQ.XpgWw9hTMm09bv1OUfz8sciMm55j_s-Rn5Hup_dFg6E";
-    private final SecretKey secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes());
-    private static long expiration = 3600000;
+    private SecretKey secretKey;
+    private long expiration = 3600000;
+
+    @Value("${jwt.secret}")
+    private String secretKeyString;
+
+    @PostConstruct
+    public void init() {
+        secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes());
+    }
 
     public String generateToken(long id, String role) {
         Date now = new Date();
