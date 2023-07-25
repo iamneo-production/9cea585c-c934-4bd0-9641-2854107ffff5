be6712f2b4261ab3ce7f09179c0bc8b83b3c1994
sugaranjan <sugaranjan06@gmail.com>
2023-07-25 12:26:58 +0000

    Integration Check

--- springapp/src/main/java/com/example/springapp/controller/PropertyController.java
+++ springapp/src/main/java/com/example/springapp/controller/PropertyController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.springapp.config.MediaFileService;
@@ -26,6 +27,7 @@ import com.example.springapp.service.PropertyService;
 
 @RestController
 @RequestMapping("/properties")
+@CrossOrigin(origins = "https://8081-dfafaaeeddfbcddcfcdcebdafbcfcbaedbffbeeaadbbb.project.examly.io", allowedHeaders = "*") 
 public class PropertyController {
 
     @Autowired
--- springapp/src/main/java/com/example/springapp/controller/QueryController.java
+++ springapp/src/main/java/com/example/springapp/controller/QueryController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.springapp.model.Query;
 import com.example.springapp.service.QueryService;
 
 @RestController
 @RequestMapping("/Query")
+@CrossOrigin(origins = "https://8081-dfafaaeeddfbcddcfcdcebdafbcfcbaedbffbeeaadbbb.project.examly.io", allowedHeaders = "*") 
 public class QueryController {
 
     @Autowired
